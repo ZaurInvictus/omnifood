43ddba4e716d7b46963eee904fadeb5599df693b
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-02-11 23:05:23 -0600

    scroll animations update

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,34 @@ $(document).ready(function () {
      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
    });
 
+
+
+   /* ANIMATION ON SCROLL */
+   $('.js--wp-1').waypoint(function(direction){
+     $('.js--wp-1').addClass('animated fadeIn');
+   }, {
+     offset: '50%'
+   });
+
+   /* ANIMATION ON SCROLL */
+   $('.js--wp-2').waypoint(function(direction){
+     $('.js--wp-2').addClass('animated fadeInUp');
+   }, {
+     offset: '50%'
+   });
+
+   /* ANIMATION ON SCROLL */
+   $('.js--wp-3').waypoint(function(direction){
+     $('.js--wp-3').addClass('animated fadeIn');
+   }, {
+     offset: '50%'
+   });
+
+   /* ANIMATION ON SCROLL */
+   $('.js--wp-4').waypoint(function(direction){
+     $('.js--wp-4').addClass('animated pulse');
+   }, {
+     offset: '50%'
+   });
+
 });
